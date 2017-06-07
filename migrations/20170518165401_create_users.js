e621eea0670f0c974b7678f19361022708d0a551
A-Tawfik <taamer@gmail.com>
2017-06-07 16:48:05 -0400

    get request for posts

--- migrations/20170518165401_create_users.js
+++ migrations/20170518165401_create_users.js
@@ -3,9 +3,10 @@ exports.up = function(knex, Promise) {
 
   return knex.schema.createTable('users', function (table) {
     table.increments('id').unsigned().primary();
-    table.string('username').notNull();
-    table.string('password').notNull();
-    table.timestamps();
+    table.string('username').unique().notNullable();
+    table.string('password').notNullable();
+    table.boolean('admin').notNullable().defaultTo(false);
+    table.timestamp('created_at').notNullable().defaultTo(knex.raw('now()'));
     table.unique('username');
 })
 
